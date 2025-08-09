363f52d620dac0a28830a43f8c5e6bb0ee335570
kaifhb <97424696+kaifhb@users.noreply.github.com>
2025-08-09 17:42:37 +0530

    imports error fixes

--- src/pages/CreateForm.tsx
+++ src/pages/CreateForm.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../store'
 import { addField, updateField, deleteField, reorderFields, saveCurrentForm, setFormName, resetDraft } from '../features/formSlice'
